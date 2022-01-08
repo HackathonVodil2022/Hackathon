8cb66812e7f5f2aee0955755b6e32b09b08c2d0d
Hackathon <97339570+HackathonVodil2022@users.noreply.github.com>
2022-01-08 16:34:11 +0500

    Blaknot codes

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,7 +1,6 @@
 //---------------------------------------------------------------------------
 
 #include <vcl.h>
-#include <cmath>
 #pragma hdrstop
 
 #include "Unit1.h"
@@ -9,191 +8,65 @@
 #pragma package(smart_init)
 #pragma resource "*.dfm"
 TForm1 *Form1;
-int a,b,c;
-int i;
 //---------------------------------------------------------------------------
 __fastcall TForm1::TForm1(TComponent* Owner)
 	: TForm(Owner)
 {
 }
 //---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton9Click(TObject *Sender)
+void __fastcall TForm1::fayl2Click(TObject *Sender)
 {
-	if(Label1->Caption==0)
-	{
-	Label1->Caption=1;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"1";
-    }
+ OpenDialog1->Execute();
+ Memo1->Lines->LoadFromFile(OpenDialog1->FileName);
 
 }
 //---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton10Click(TObject *Sender)
+void __fastcall TForm1::saqlash1Click(TObject *Sender)
 {
-if(Label1->Caption==0)
-	{
-	Label1->Caption=2;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"2";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton11Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=3;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"3";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton5Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=4;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"4";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton6Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=5;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"5";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton7Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=6;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"6";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton1Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=7;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"7";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton2Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=8;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"8";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton3Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=9;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"9";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton13Click(TObject *Sender)
-{
-if(Label1->Caption==0)
-	{
-	Label1->Caption=0;
-	}
-	else
-	{
-		Label1->Caption=Label1->Caption+"0";
-	}
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton15Click(TObject *Sender)
-{
-Label1->Caption=0;
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton4Click(TObject *Sender)
-{
-a=StrToInt(Label1->Caption);
-Label1->Caption=0;
-i=1;
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton8Click(TObject *Sender)
-{
-a=StrToInt(Label1->Caption);
-Label1->Caption=0;
-i=2;
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton12Click(TObject *Sender)
-{
-a=StrToInt(Label1->Caption);
-Label1->Caption=0;
-i=3;
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton16Click(TObject *Sender)
-{
-a=StrToInt(Label1->Caption);
-Label1->Caption=0;
-i=4;
-}
-//---------------------------------------------------------------------------
-void __fastcall TForm1::SpeedButton14Click(TObject *Sender)
-{
-b=StrToInt(Label1->Caption);
-if(i==1)
-	{
-	c=a+b;
-	}
-		if(i==2)
-		{
-		c=a-b;
-		}
-if(i==3)
-{
-c=a*b;
-}
-	if(i==4)
-	{
-	c=a/b;
-	}
-Label1->Caption=c;
+SaveDialog1->Execute();
+Memo1->Lines->SaveToFile(SaveDialog1->FileName);
 
 }
 //---------------------------------------------------------------------------
-
-
-
+void __fastcall TForm1::pechat1Click(TObject *Sender)
+{
+PrintDialog1->Execute();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::yopish1Click(TObject *Sender)
+{
+Form1->Close();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::shrift1Click(TObject *Sender)
+{
+FontDialog1->Execute();
+Memo1->Font=FontDialog1->Font;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::shrift2Click(TObject *Sender)
+{
+ColorDialog1->Execute();
+Memo1->Color=ColorDialog1->Color;
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::belgilsh1Click(TObject *Sender)
+{
+Memo1->SelectAll();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::belgilsh2Click(TObject *Sender)
+{
+Memo1->CopyToClipboard();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::qoyish1Click(TObject *Sender)
+{
+Memo1->PasteFromClipboard();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::tozalash1Click(TObject *Sender)
+{
+Memo1->Clear();
+}
+//---------------------------------------------------------------------------
